3a22e7c9885b110fe6e152e1a26e477545585ad6
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2023-04-17 16:59:54 -0700

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,7 +4,7 @@
 /*
  * File: holds the formula
  * Desc:  a function-like macro ABS(x)that computes the
- *absolute value of a number x.
+ * absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))

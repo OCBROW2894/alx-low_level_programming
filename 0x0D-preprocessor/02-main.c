84c5e12df4c3533c87898d1f135a5257289fc7f4
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2023-04-17 16:26:41 -0700

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - where the compiler starts compiling
+ * Description: print it's name
+ * Return: Always 0 when correct.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

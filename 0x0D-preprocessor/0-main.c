adbc919b7fbd9c03caef0945652bb256a97f319a
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2023-04-17 16:12:12 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - where the compiler starts compiling the code
+ *
+ * Return: Always 0 when correct.
+ */
+int main(void)
+{
+	int bryian;
+
+	bryian = 98 + SIZE;
+	printf("%d\n", bryian);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: holds rhe marco size info
+ * define: Defines a macro named SIZE of value
+ */
+
+#define SIZE 1024
+
+#endif

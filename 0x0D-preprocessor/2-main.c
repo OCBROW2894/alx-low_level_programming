f9cf786bca055ffa55e5c6311d48fa4df8174f9f
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2023-04-17 16:41:44 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's self
+ * Description: print the name of the source file
+ * Return: Always 0 when correct.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

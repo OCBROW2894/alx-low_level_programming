fdc7ae7cb00fb69500fd5dff3d9a3ee08ef2c755
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2023-04-17 17:22:42 -0700

    SUM

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "4-sum.h"
+
+/**
+ * main - where the copiler starts compiling
+ *
+ * Return: Always 0 when correct.
+ */
+int main(void)
+{
+	int answer;
+
+	answer = SUM(2800, 94);
+	printf("%d\n", answer);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef SUM_H
+#define SUM_H
+
+ /*
+  * self explanatory
+  */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif

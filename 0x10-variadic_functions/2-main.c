aa2df1c0425234ae07849f2142aad8d5574688f5
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2023-04-20 17:08:44 -0700

    2. One woman's constant is another woman's variable

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0 when correct.
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Bryian", "Ocbrow");
+	return (0);
+}

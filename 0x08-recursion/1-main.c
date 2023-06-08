0c9d6be78aec4756b991570648e4a90d2c756332
OCBROW2894 <ochiengbryianowour2894@gmail.com>
2023-06-08 11:15:51 -0700

    1. Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - compiler starts compiling the code
+ *
+ * Return: Always 0 when correct.
+ */
+int main(void)
+{
+	_print_rev_recursion("\nOchieng Bryian Owuor");
+	return (0);
+}

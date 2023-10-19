197a7d78cefdb71d84f6add4929527234847a6e9
OCBROW2894 <ochiengbryianowour2894@gmail.com>
2023-10-19 12:40:19 -0700

    The rest of the work including the Advanced tasks

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - compiler starts copilling the code
+ *
+ * Return: Always 0 when correct.
+ */
+int main(void)
+{
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}

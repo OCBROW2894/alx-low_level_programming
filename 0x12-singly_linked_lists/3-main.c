dbd6ef9a03e6a396d61f67db425b24ec4f984059
OCBROW2894 <ochiengbryianowour2894@gmail.com>
2023-10-19 12:26:07 -0700

    3. Add node at the end

---
+++ 0x12-singly_linked_lists/3-main.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
+
+    head = NULL;
+    add_node_end(&head, "Obito");
+    add_node_end(&head, "Itachi");
+    add_node_end(&head, "Dedara");
+    add_node_end(&head, "Pain");
+    add_node_end(&head, "Konan");
+    add_node_end(&head, "Bryian");
+    add_node_end(&head, "Madara");
+    add_node_end(&head, "Hashirama");
+    add_node_end(&head, "Ian");
+    add_node_end(&head, "Ian");
+    add_node_end(&head, "Jay");
+    add_node_end(&head, "Jennie");
+    add_node_end(&head, "Jimmy");
+    add_node_end(&head, "Justin");
+    add_node_end(&head, "Kalson");
+    add_node_end(&head, "Kina");
+    add_node_end(&head, "Matthew");
+    add_node_end(&head, "Max");
+    add_node_end(&head, "Michael");
+    add_node_end(&head, "Ntuj");
+    add_node_end(&head, "Philip");
+    add_node_end(&head, "Richard");
+    add_node_end(&head, "Samantha");
+    add_node_end(&head, "Stuart");
+    add_node_end(&head, "Swati");
+    add_node_end(&head, "Timothy");
+    add_node_end(&head, "Victor");
+    add_node_end(&head, "Walton");
+    print_list(head);
+    return (0);
+}

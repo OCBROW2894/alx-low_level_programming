a256f4abe921f315c86944ce75f51b527e29f05b
OCBROW2894 <ochiengbryianowour2894@gmail.com>
2023-10-24 10:40:25 -0700

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 when correct.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 2012);
+    add_nodeint_end(&head, 2749);
+    add_nodeint_end(&head, 2894);
+    print_listint(head);
+    return (0);
+}
+

c54f03485d250adf489bafdc9cb5f58485cd9e76
OCBROW2894 <ochiengbryianowour2894@gmail.com>
2023-10-24 10:49:20 -0700

    6. Pop

---
+++ 0x13-more_singly_linked_lists/6-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 when correct.
+ */
+int main(void)
+{
+    listint_t *head;
+    int n;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 2012);
+    add_nodeint_end(&head, 2749);
+    add_nodeint_end(&head, 2894);
+    print_listint(head);
+    n = pop_listint(&head);
+    printf("- %d\n", n);
+    print_listint(head);
+    n = pop_listint(&head);
+    printf("- %d\n", n);
+    print_listint(head);
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
+    return (0);
+}

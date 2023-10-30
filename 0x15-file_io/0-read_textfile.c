4fe09c93252fb3178d044864d8aff62467be2958
OCBROW2894 <ochiengbryianowour2894@gmail.com>
2023-10-30 13:51:13 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,51 @@
 #include "main.h"
 
 /**
- * read_textfile - Read a text file and print to STDOUT
- * @filename: File name pointer
- * @letters: Number of characters to read
- * Return: 0 when file name is NULL
+ * read_textfile - reads a text file and prints to the STDOUT
+ * @filename: name of the file to read
+ * @letters: maximum number of letters to be read and printed
+ *
+ * Return: actual number of letters it could read and print
+ * if filename is NULL return 0
+ * if write fails return 0
  */
- 
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t op, rd, wr;
+	int fd, length, i, result;
 	char *buffer;
 
+	/*check if the parameter is NULL*/
 	if (filename == NULL)
 		return (0);
 
+	/*open the file in read only mode*/
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	/*allocate a buffer of size letters*/
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	op = open(filename, O_RDONLY);
-	rd = read(op, buffer, letters);
-	wr = write(STDOUT_FILENO, buffer, rd);
+	/*read and add a null terminator*/
+	read(fd, buffer, letters);
+	buffer[letters] = '\0';
 
-	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
-	{
-		free(buffer);
+	for (i = 0; buffer[i] != '\0'; i++)
+		length += 1;
+
+	result = close(fd);
+	if (result != 0)
+		exit(-1);
+
+	/*write contents of buffer to STDOUT*/
+	result = write(STDOUT_FILENO, buffer, length);
+	if (result != length)
 		return (0);
-	}
 
 	free(buffer);
-	close(op);
 
-	return (wr);
+	return (length);
 }
